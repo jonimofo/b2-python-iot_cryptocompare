8bbc588d1279b1342a53c8cc60f4b083678b48ba
vagrant <vagrant@debian-85-amd64>
2018-03-25 12:39:58 -0400

    color and syntax fixing

--- cryto-compare/GIRALT_BENJAMIN_cryptocompare.py
+++ cryto-compare/GIRALT_BENJAMIN_cryptocompare.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3.5 
-# -*- coding: utf-8 -*-@
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------------------------------------  
+# 	purpose		| simple script using cryptocompy api 
+#      			|   type list to get the list of all crypto currencies available
+#      			|   type price to get the exchange rate of a given cryptocurrency in euros
+# 	author		| Benjamin GIRALT - B2A 
+# 	date		| 26/03/18
+# 	version		| 01.0
+# --------------------------------------------------------------------------------------------------------- 
 
 # 1. Demande à l'utilisateur la crypto dont il veut le prix 
     #  ou s'il veut la liste des cryptos disponibles.
@@ -10,14 +18,13 @@
 # 3. Retour au n°1
 
 
-from cryptocompy import coin
+from cryptocompy import coin, price 
 import os
 import sys
-import colors
+import ansi_colors as colors
 
 
-list_crypto = coin.get_coin_list()
-
+list_cryptocurrencies = coin.get_coin_list()
 
 
 def clear_screen() -> None:
@@ -26,52 +33,74 @@ def clear_screen() -> None:
 
 
 
-def get_list_crypto(list_crypto):
+def get_list_cryptocurrencies(list):
+    l = ''
     
-    for key, value in list_crypto.items():
+    for key, value in list.items():
+        print(value.get('CoinName') ,'- (',value.get('Name'),')')
+    
+    print('\n{}{} crypto currencies found.{}\n'.format(colors.GREEN, len(list), colors.ESCAPE))
 
-        return (value.get('FullName'))
+
+
+def get_selling_price(cryptocurrency, currency):
+
+    p = price.get_current_price(cryptocurrency, ["EUR"])
+    selling_price = p[cryptocurrency][currency]
+    
+    return ('\n{} = {} {}\n'.format(cryptocurrency, selling_price, currency))
 
 
 
 def process_user_choice(user_choice):
 
-    if user_choice == 1:
+    if user_choice == 'list':
+        print('\n')
+        get_list_cryptocurrencies(list_cryptocurrencies)
 
-        get_list_crypto(list_crypto)
-
-    elif user_choice == 0:
-
+    elif user_choice == 'quit':
         # TODO print(successfully exited with code 0)
         exit
+    
+    elif user_choice == 'price':
+
+        while True:
+            print('\n{}Please enter the desired cryptocurrency. e.g : BTC for Bitcoin{}'.format(colors.YELLOW, colors.ESCAPE))
+            crypto = str.upper((input(' > ')))
+
+            try:
+                print(get_selling_price(crypto, "EUR"))
+                break
+            except:
+                print('{}\nInvalid cryptocurrency, please enter a correct one{}'.format(colors.RED, colors.ESCAPE))
+                
 
     else :
-
-        print('toto')
+        print('Syntax Error.')
 
 
 
 def main():
     
-    clear_screen()
+    clear_screen() 
+    possible_inputs = ['list', 'price', 'quit']
 
-    print('{}Number of cryptocurrencies available : {}{}\n'.format(colors.YELLOW, len(list_crypto), colors.ESCAPE))
-    print('{}Enter 1 to get a list of all available cryptocurrencies{}\n'.format(colors.CYAN, colors.ESCAPE))
-    print('{}Enter 0 to quit the program{}'.format(colors.CYAN, colors.ESCAPE))
+    # print('{}Number of cryptocurrencies available : {}{}\n'.format(colors.YELLOW, len(list_cryptocurrencies), colors.ESCAPE))
+    print('{}\nlist -> {}Get the list of all available cryptocurrencies\n'.format(colors.YELLOW, colors.ESCAPE))
+    print('{}price -> {}Get the price in EUROS of a given cryptocurrency\n'.format(colors.YELLOW, colors.ESCAPE))
+    print('{}\nquit -> {}Exit the program\n'.format(colors.YELLOW, colors.ESCAPE))
 
-    # TODO : verifier valeurs / unwanted values pour ne pas quitter si mauvais choix
-    try:
+    while True:
+        user_choice = (input('\n{}Your choice :{} \n > '.format(colors.CYAN, colors.ESCAPE)))
 
-        user_choice = int(input('\nYour choice \n > ').format(colors.CYAN, colors.ESCAPE))
+        if not user_choice in possible_inputs:
 
-    except ValueError:
+            print('{}\nSyntax error. Please enter your choice again{}'.format(colors.RED, colors.ESCAPE))
+        
+        else:
 
-        sys.stderr.write("Error : Undefined choice\n")
-        sys.exit(1)
-
-    process_user_choice(user_choice)
-
+            process_user_choice(user_choice)
+            break
 
 
 main()
-
